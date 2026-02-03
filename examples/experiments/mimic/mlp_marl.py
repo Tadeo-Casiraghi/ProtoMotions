222172f3163ad99c6e1a9e8cf7eb2081eeed46df
Tadeo-Casiraghi <tadeo.casiraghi@gmail.com>
2026-02-03 10:39:22 -0300

    Auto stash before merge of "main" and "origin/main"

--- examples/experiments/mimic/mlp_marl.py
+++ examples/experiments/mimic/mlp_marl.py
@@ -164,10 +164,10 @@ def env_config(robot_cfg: RobotConfig, args: argparse.Namespace) -> MimicEnvConf
 #         training_max_steps=args.training_max_steps,
 #         num_steps=32,
         
-        # Orchestrator Settings
-        mapping_info=mapping_info,
-        agents={
-            "humanoid": create_ppo_config("humanoid", BODY_DOFS),
-            "leg":      create_ppo_config("leg", LEG_DOFS)
-        }
-    )
+#         # Orchestrator Settings
+#         mapping_info=mapping_info,
+#         agents={
+#             "humanoid": create_ppo_config("humanoid", BODY_DOFS),
+#             "leg":      create_ppo_config("leg", LEG_DOFS)
+#         }
+#     )
